c0e741a952072c9f5e0c50f64bc8a1952f9acb8d
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2025-04-10 16:21:54 +0200

    implementation performance test

--- src/main/java/pcd/ass01/TaskBoidSimulator.java
+++ src/main/java/pcd/ass01/TaskBoidSimulator.java
@@ -3,91 +3,86 @@ package pcd.ass01;
 
 import pcd.ass01.task.PositionUpdateTask;
 import pcd.ass01.task.VelocityUpdateTask;
-import pcd.ass01.utility.SyncSuspension;
-import pcd.ass01.utility.SyncRunning;
-import pcd.ass01.utility.SafeBoolean;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.*;
 
 public class TaskBoidSimulator {
-    private static final int N_THREAD = Runtime.getRuntime().availableProcessors() + 1;
-    private static final int FRAMERATE = 25;
+    private static final int N_THREAD = 1;
+    //private static final int FRAMERATE = 25;
 
     private BoidsModel model;
-    private Barrier initialHandshackePoint;
-    private Barrier finalHandshakePoint;
 
-    private SyncRunning runningMonitor;
-    private SyncSuspension suspensionMonitor;
+    //private SyncRunning runningMonitor;
+    //private SyncSuspension suspensionMonitor;
 
     private ExecutorService executor;
     private int nBoids;
-    private int framerate;
-    private Optional<BoidsView> view;
+    //private int framerate;
+    //private Optional<BoidsView> view;
 
-    private final  SafeBoolean isStopped;
+    private int nFrames;
+
+    //private final  SafeBoolean isStopped;
 
     public TaskBoidSimulator(BoidsModel model) {
         this.model = model;
-        this.view = Optional.empty();
-        this.initialHandshackePoint = new Barrier(2);
-        this.finalHandshakePoint = new Barrier(2);
-        this.runningMonitor = new SyncRunning();
-        this.suspensionMonitor = new SyncSuspension();
+        //this.view = Optional.empty();
+        //this.initialHandshackePoint = new Barrier(2);
+        //this.finalHandshakePoint = new Barrier(2);
+        //this.runningMonitor = new SyncRunning();
+        //this.suspensionMonitor = new SyncSuspension();
         this.executor = Executors.newFixedThreadPool(N_THREAD);
-        this.isStopped = new SafeBoolean();
+        //this.isStopped = new SafeBoolean();
     }
 
     public BoidsModel getModel(){
         return this.model;
     }
 
-    public void attachView(BoidsView view) {
+    /*public void attachView(BoidsView view) {
         this.view = Optional.of(view);
-    }
+    }*/
 
-    public void startSimulation(int nBoids){
-        this.executor = Executors.newCachedThreadPool();
+    public void startSimulation(int nBoids, int nFrames){
+        //this.executor = Executors.newCachedThreadPool();
         this.nBoids = nBoids;
-        try {
+        this.nFrames = nFrames;
+        /*try {
             this.initialHandshackePoint.notifyJobDone();
             this.finalHandshakePoint.notifyJobDone();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     public void run(){
-        while(true){
-            try {
-                this.initialHandshackePoint.notifyJobDone();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            this.model.createSimulation(this.nBoids);
-            try {
-                this.finalHandshakePoint.notifyJobDone();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        /*try {
+            this.initialHandshackePoint.notifyJobDone();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
+        this.model.createSimulation(this.nBoids);
+        /*try {
+            this.finalHandshakePoint.notifyJobDone();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
 
-            this.isStopped.setFalse();
-            this.runningMonitor.active();
+        //this.isStopped.setFalse();
+        //this.runningMonitor.active();
 
-            this.runSimulation();
-        }
+        this.runSimulation();
     }
 
     public void runSimulation() {
-        while (!isStopped.get()) {
-            var t0 = System.currentTimeMillis();
-            suspensionMonitor.suspensionUntilResume();
+        for (int i = 0; i < nFrames - 1; i++) {
+            //var t0 = System.currentTimeMillis();
+            //suspensionMonitor.suspensionUntilResume();
             stateUpdate();
-            GUIUpdate(t0);
-            runningMonitor.waitIfStopped();
+            //GUIUpdate(t0);
+            //runningMonitor.waitIfStopped();
         }
     }
 
@@ -125,10 +120,10 @@ public class TaskBoidSimulator {
             throw new RuntimeException(e);
         }
     }
-
+/* 
     public void GUIUpdate(final long t0) {
-        if (view.isPresent()) {
-            view.get().update(framerate);
+        //if (view.isPresent()) {
+            //view.get().update(framerate);
             var t1 = System.currentTimeMillis();
             var dtElapsed = t1 - t0;
             var framePeriod = 1000 / FRAMERATE;
@@ -143,15 +138,15 @@ public class TaskBoidSimulator {
             } else {
                 framerate = (int) (1000 / dtElapsed);
             }
-        }
-    }
+        //}
+    }*/
 
     public void toggleSuspendResume() {
-        if (view.isPresent()) {
-            final String s = view.get().getSuspendResumeButtonText();
-            view.get().updateSuspendResumeButtonText(s.equals("Resume") ? "Suspend" : "Resume");
-        }
-        suspensionMonitor.changeState();
+        //if (view.isPresent()) {
+            //final String s = view.get().getSuspendResumeButtonText();
+            //view.get().updateSuspendResumeButtonText(s.equals("Resume") ? "Suspend" : "Resume");
+        //}
+        //suspensionMonitor.changeState();
     }
 
     public void stopSimulation() {
@@ -162,16 +157,16 @@ public class TaskBoidSimulator {
             throw new RuntimeException(e);
         }
 
-        runningMonitor.stop();
+        //runningMonitor.stop();
 
-        runningMonitor.waitUntilArriveWait();
+        //runningMonitor.waitUntilArriveWait();
 
-        isStopped.setTrue();
+        //isStopped.setTrue();
 
-        if (view.isPresent()) {
+        /*if (view.isPresent()) {
             view.get().resetToInitialScreen();
-        }
-        runningMonitor.active();
+        }*/
+        //runningMonitor.active();
     }
 
     public void setSeparationWeight(double value) {
